7809f62959c9d3ab2c7447027dc55d54d4e6aa2f
Nil4s3 <nicolasteschima28@gmail.com>
2022-08-02 22:09:29 -0400

    removido contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 # domain/recipes/
 urlpatterns = [
     path('', home),  # home
-    path('sobre/', sobre),  # about
-    path('contato/', contato),  # contact
+
 
 ]
