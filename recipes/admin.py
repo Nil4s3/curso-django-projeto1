02bbdab5437c493c1e51c5fe2b75efcffc4ccf19
Nil4s3 <nicolasteschima28@gmail.com>
2022-08-08 18:53:33 -0400

    migrations, filters, updates

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Recipe
+
+
+class CatergoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CatergoryAdmin)
