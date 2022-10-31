e16257d86b6647c864cf701bb9f8633046dece77
Nicolas <nicolasteschima28@gmail.com>
2022-10-31 15:36:30 -0300

    Updates

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,25 @@
-from django.shortcuts import get_list_or_404, get_object_or_404, render
-from utils.recipes.factory import make_recipe
+import os
 
-from .models import Recipe
+from django.db.models import Q
+from django.http.response import Http404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from utils.pagination import make_pagination
+
+from recipes.models import Recipe
+
+PER_PAGE = os.environ.get('PER_PAGE', 6)
 
 
 def home(request):
-    recipes = get_list_or_404(
-        Recipe.objects.filter(
-            is_published=True,
-        ).order_by('-id')
-    )
+    recipes = Recipe.objects.filter(
+        is_published=True,
+    ).order_by('-id')
+
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range
     })
 
 
@@ -24,8 +32,11 @@ def category(request, category_id):
         ).order_by('-id')
     )
 
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
         'title': f'{recipes[0].category.name} - Category | '
     })
 
@@ -40,4 +51,25 @@ def recipe(request, id):
 
 
 def search(request):
-    return render(request, 'recipes/pages/search.html')
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={search_term}',
+    })
