e0b9c8f91c4dcdbf3404612d506007a7ba9a3d0d
Nicolas <nicolasteschima28@gmail.com>
2022-10-31 15:35:31 -0300

    Updates

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -123,3 +123,17 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipe_search_loads_correct_template(self):
         response = self.client.get(reverse('recipes:search'))
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
+
+    def test_recipe_search_raises_404_if_no_search_term(self):
+        url = reverse('recipes:search')
+        response = self.client.get(url)
+        self.asserEqual(response.status_code, 404)
+    
+    def test_recipe_search_term_is_on_page_title_and_escaped(self):
+        url = reverse('recipes:search') + '?q=<Teste>'
+        response = self.client.get(url)
+        self.assertIn(
+            'Search for &quot;&lt;Test&gt;&quot;',
+            response.content.decode('utf-8')
+        )
+    
