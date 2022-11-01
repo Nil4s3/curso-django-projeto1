28416bdb9630bc80115ee7a45ada53a468d6654d
Nicolas <nicolasteschima28@gmail.com>
2022-11-01 15:17:07 -0300

    update

--- tests/recipes/test_recipe_home_page.py
+++ tests/recipes/test_recipe_home_page.py
@@ -1,10 +1,31 @@
+from unittest.mock import patch
+
+import pytest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 from .base import RecipeBaseFunctionalTest
 
 
+@pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+    @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
-        self.assertIn('No recipes found here.', body.text)
+        self.assertIn('No recipes found here .', body.text)
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_search_input_can_find_correct_recipes(self):
+        # user open pages
+        self.browser.get(self.live_server_url)
+
+        # see a search bar
+        search_input = self.browser.find_element(
+            By.XPATH,
+            '//input[@placeholder="Search for a recipe"]'
+        )
+        search_input.send_keys('Recipe title 1')
+        search_input.send_keys(Keys.ENTER)
+
+        self.sleep(6)
