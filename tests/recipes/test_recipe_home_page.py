f2723d618d064ed62b8d32e1dfb33849e8b58bd4
Nicolas <nicolasteschima28@gmail.com>
2022-11-01 16:10:31 -0300

    bug fixes

--- tests/recipes/test_recipe_home_page.py
+++ tests/recipes/test_recipe_home_page.py
@@ -9,7 +9,6 @@ from .base import RecipeBaseFunctionalTest
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
-    @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
@@ -17,15 +16,38 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
 
     @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_search_input_can_find_correct_recipes(self):
-        # user open pages
+        recipes = self.make_recipe_in_batch()
+
+        title_needed = 'This is what I need'
+
+        recipes[0].title = title_needed
+        recipes[0].save()
+
+        # Usuário abre a página
         self.browser.get(self.live_server_url)
 
-        # see a search bar
+        # Vê um campo de busca com o texto "Search for a recipe"
         search_input = self.browser.find_element(
             By.XPATH,
             '//input[@placeholder="Search for a recipe"]'
         )
-        search_input.send_keys('Recipe title 1')
+
+        # Clica neste input e digita o termo de busca
+        # para encontrar a receita o título desejado
+        search_input.send_keys(title_needed)
         search_input.send_keys(Keys.ENTER)
 
-        self.sleep(6)
+        # O usuário vê o que estava procurando na página
+        self.assertIn(
+            title_needed,
+            self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
+        )
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_home_page_pagination(self):
+        recipes = self.make_recipe_in_batch()
+
+        # Usuario abre a pagina
+        self.browser.get(self.live_server_url)
+
+        self.sleep(10)
