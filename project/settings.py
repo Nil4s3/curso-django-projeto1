1e3abb4280d1ca5f476003fc4db52cbb57e6bf9b
Nicolas <nicolasteschima28@gmail.com>
2022-11-02 20:30:30 -0300

    Django debug toolbar

--- project/settings.py
+++ project/settings.py
@@ -43,16 +43,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'recipes',
     'authors',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -145,3 +148,8 @@ MESSAGE_TAGS = {
     constants.WARNING: 'message-warining',
 
 }
+
+#
+INTERNAL_IPS = [
+    "127.0.0.1"
+]
--- recipes/urls.py
+++ recipes/urls.py
@@ -6,12 +6,32 @@ app_name = 'recipes'
 
 # domain/recipes/
 urlpatterns = [
-    path('', views.RecipeListViewHome.as_view(), name="home"),
+    path(
+        '',
+        views.RecipeListViewHome.as_view(),
+        name="home"
+    ),
     path(
         'recipes/search/',
         views.RecipeListViewSearch.as_view(), name="search"
     ),
-    path('recipes/category/<int:category_id>/',
-         views.RecipeListViewCategory.as_view(), name="category"),
-    path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name="recipe"),
+    path(
+        'recipes/category/<int:category_id>/',
+        views.RecipeListViewCategory.as_view(),
+        name="category"),
+    path(
+        'recipes/<int:pk>/',
+        views.RecipeDetail.as_view(),
+        name="recipe"
+    ),
+    path(
+        'recipes/api/v1/',
+        views.RecipeListViewHomeApi.as_view(),
+        name="recipes_api_v1"
+    ),
+    path(
+        'recipes/api/v1/<int:pk>',
+        views.RecipeDetailAPI.as_view(),
+        name="recipes_api_v1_detail"
+    ),
 ]
