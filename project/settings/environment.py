60331bfedba801be191033496dd63a8a25864d97
Nicolas <nicolasteschima28@gmail.com>
2022-11-03 17:20:50 -0300

    confgs update

---
+++ project/settings/environment.py
@@ -0,0 +1,17 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
+
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
+
+
+ALLOWED_HOSTS = []  # type: ignore
+
+ROOT_URLCONF = 'project.urls'
+
+WSGI_APPLICATION = 'project.wsgi.application'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
